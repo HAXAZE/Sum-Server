3f56853d80a6db9ede4e705289bff03737f15b1b
Sahil kumar <28SKOFFICIAL01@GMAIL.COM>
2023-12-27 22:24:53 +0530

    server modification

--- server.js
+++ server.js
@@ -7,4 +7,16 @@ app.get("/findsum",function(req,res){
     const sum = numa + numb;
     res.send(sum.toString());
 });
+
+app.get("/interest",function(req,res){
+    const principal = parseInt(req.query.principal);
+    const rate = parseInt(req.query.rate);
+    const time = parseInt(req.query.time);
+    const interest = (principal * rate * time)/100;
+    const total = principal + interest;
+    res.send({
+        total:total,
+        interest:interest,
+    })
+})
 app.listen(8080);
